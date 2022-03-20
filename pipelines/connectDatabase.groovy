368b2ddd63c1d6e26562d2bff6cb3346dbd83356
sougatadas10 <sougatadas10@users.noreply.github.com>
2022-03-20 11:53:18 +0530

    Create connectDatabase.groovy

---
+++ pipelines/connectDatabase.groovy
@@ -0,0 +1,22 @@
+//@Library('sharedLibrary')_
+//import com.foo.DBConnection;
+import groovy.sql.Sql;
+
+//def db=new DBConnection()
+
+
+node() {
+  stage ('connection') {
+    def classLoader = ClassLoader.systemClassLoader
+    while (classLoader.parent) {
+        classLoader = classLoader.parent
+    }
+    classLoader.addURL(new File("$WORKSPACE/resources/mysql-connector-java-8.0.28.jar").toURL())
+    def sql = Sql.newInstance("jdbc:mysql://host.docker.internal:3306/employee", "root", "root@pass", "com.mysql.jdbc.Driver")
+    sql.firstRow('SELECT employee_id, employee_name FROM employee_master')
+    sql.close()
+  }
+        
+	
+}
+    
