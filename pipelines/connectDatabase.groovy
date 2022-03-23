8224d2119c5000bec75e90f0c4a999166e851866
sougatadas10 <sougatadas10@users.noreply.github.com>
2022-03-24 02:13:19 +0530

    Update connectDatabase.groovy

--- pipelines/connectDatabase.groovy
+++ pipelines/connectDatabase.groovy
@@ -11,14 +11,15 @@ node() {
      git branch: 'main', changelog: false, poll: false, url: 'https://github.com/sougatadas10/pipelines.git'
   }	
   stage ('connection') {
-    def classLoader = ClassLoader.systemClassLoader
+    /**def classLoader = ClassLoader.systemClassLoader
     while (classLoader.parent) {
         classLoader = classLoader.parent
     }
     classLoader.addURL(new File("$WORKSPACE/resources/mysql-connector-java-8.0.28.jar").toURL())
     def sql = Sql.newInstance("jdbc:mysql://host.docker.internal:3306/employee", "root", "root@pass", "com.mysql.jdbc.Driver")
     sql.firstRow('SELECT employee_id, employee_name FROM employee_master')
-    sql.close()
+    sql.close()**
+    db.getConnection(this)
   }
         
 	
