275e25ac03dd93f32c830c41c6048480be4fdb66
sougatadas10 <sougatadas10@users.noreply.github.com>
2022-03-30 09:29:23 +0530

    Create seedjob.groovy

---
+++ seedjob/seedjob.groovy
@@ -0,0 +1,28 @@
+pipelineJob('DSL_Pipeline') {
+
+  def repo = 'https://github.com/sougatadas10/infra.git'
+/** Scheduling job
+  triggers {
+    scm('H/5 * * * *')
+  } **/
+  description("Pipeline for $repo")
+
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote { url(repo) }
+          //branches('master', '**/feature*') Can be used if we have pipelines in different branch
+          
+          scriptPath('pipelines/connectDatabase.groovy')
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+        }
+
+        // the single line below also works, but it
+        // only covers the 'master' branch and may not give you
+        // enough control.
+        // git(repo, 'master', { node -> node / 'extensions' << '' } )
+      }
+    }
+  }
+}
