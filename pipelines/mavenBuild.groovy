fe370b77af4c9363372aeff1a410841f3c16a7b1
sougatadas10 <sougatadas10@users.noreply.github.com>
2022-03-31 22:36:14 +0530

    Update mavenBuild.groovy

--- pipelines/mavenBuild.groovy
+++ pipelines/mavenBuild.groovy
@@ -1,11 +1,5 @@
 @Library('sharedLibrary')_
 
-properties([
-		buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '2',daysToKeepStr: '',numToKeepStr: '2')),
-		parameters([
-			string(name: 'commit_id',defaultValue: '',description: 'commit id' )
-			])
-	])
 
 mavenBuild(branch: "main",repo: "https://github.com/devopshint/jenkins-pipeline-example.git",file: "./my-app/pom.xml",args: "-B -DskipTests clean package")
       
