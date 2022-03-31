7cb202c1333acbe0b75bc77f7965f953916390ba
sougatadas10 <sougatadas10@users.noreply.github.com>
2022-03-31 23:41:29 +0530

    Update mavenBuild.groovy

--- pipelines/mavenBuild.groovy
+++ pipelines/mavenBuild.groovy
@@ -1,6 +1,6 @@
 @Library('sharedLibrary')_
 
 
-mavenBuild(repo: "https://github.com/devopshint/jenkins-pipeline-example.git",file: "./my-app/pom.xml",args: "-B -DskipTests clean package")
+mavenBuild(repo: "https://github.com/devopshint/jenkins-pipeline-example.git",file: "./my-app/pom.xml",args: "-B -DskipTests clean package",body)
       
 
