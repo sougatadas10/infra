82ab7569d3e6109218865718d15d222740a05a37
sougatadas10 <sougatadas10@users.noreply.github.com>
2022-03-31 21:21:22 +0530

    Update mavenBuild.groovy

--- pipelines/mavenBuild.groovy
+++ pipelines/mavenBuild.groovy
@@ -1,14 +1,2 @@
 @Library('sharedLibrary')_
-import com.foo.mvnBuild
-
-def build = new mvnBuild(this)
-
-node(){
-    stage('clone') {
-		
-		gitCheckout(branch: 'main',url: "https://github.com/devopshint/jenkins-pipeline-example.git")
-	}
-	stage('build') {
-	    build.mvn("${workspace}/my-app/pom.xml","-B -DskipTests clean package")
-	}
-}
+mavenBuild(branch: "main",repo: "https://github.com/devopshint/jenkins-pipeline-example.git",file: "./my-app/pom.xml",args: "-B -DskipTests clean package")
