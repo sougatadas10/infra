ca5280139419677bf6e79c4eb165501f4548a044
sougatadas10 <sougatadas10@users.noreply.github.com>
2022-04-01 00:46:10 +0530

    Update mavenBuild.groovy

--- pipelines/mavenBuild.groovy
+++ pipelines/mavenBuild.groovy
@@ -3,11 +3,12 @@
 properties([
     buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '2',daysToKeepStr: '',numToKeepStr: '2')),  
     parameters([
-        string(name: 'branch', defaultValue: ' ', description: 'commit id'),
-        string(name: 'repo', defaultValue: ' ', description: 'github repo'),
-        string(name: 'file', defaultValue: ' ', description: 'POM file name'),
+        string(name: 'branch', defaultValue: 'main ', description: 'commit id'),
+        string(name: 'repo', defaultValue: 'https://github.com/devopshint/jenkins-pipeline-example.git', description: 'github repo'),
+        string(name: 'file', defaultValue: 'my-app/pom.xml', description: 'POM file name'),
         string(name: 'options', defaultValue: '-B -DskipTests clean package', description: 'options'),
         
     ])
 ])
-mavenBuild(params,this)
+//params is of java.util.Collections$UnmodifiableMap
+mavenBuild(params)
