ea89bceec18da48746d4102ca9cad8dfadc3bd3c
Sougata Das <sougata.das5@cognizant.com>
2022-08-26 05:41:21 +0000

    add jobs to seedjob

--- seedjob/seedjob.groovy
+++ seedjob/seedjob.groovy
@@ -24,5 +24,59 @@ pipelineJob('example_pipeline') {
         // git(repo, 'master', { node -> node / 'extensions' << '' } )
       }
     }
+    
+    cpsScm {
+      scm {
+        git {
+          remote { url(repo) }
+          //branches('master', '**/feature*') Can be used if we have pipelines in different branch
+          branches('main')
+          scriptPath('pipelines/mavenBuild.groovy')
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+        }
+
+      }
+    }
+
+    cpsScm {
+      scm {
+        git {
+          remote { url(repo) }
+          //branches('master', '**/feature*') Can be used if we have pipelines in different branch
+          branches('main')
+          scriptPath('pipelines/multibranch-readYAML.groovy')
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+        }
+      }
+    }
+
+    cpsScm {
+      scm {
+        git {
+          remote { url(repo) }
+          //branches('master', '**/feature*') Can be used if we have pipelines in different branch
+          branches('main')
+          scriptPath('pipelines/readYAMLfile.groovy')
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+        }
+
+      }
+    }
+
+    cpsScm {
+      scm {
+        git {
+          remote { url(repo) }
+          //branches('master', '**/feature*') Can be used if we have pipelines in different branch
+          branches('main')
+          scriptPath('pipelines/runAnsible.groovy')
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+        }
+
+      }
+    }
+
+
+
   }
 }
