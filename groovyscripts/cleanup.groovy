2343f8be5159c67dd4cf44b5187f94f5735f1a41
Sougata Das <sougata.das5@cognizant.com>
2022-08-29 13:45:28 +0000

    add workspace cleanup scripts

---
+++ groovyscripts/cleanup.groovy
@@ -0,0 +1,22 @@
+import hudson.model.*
+  
+def deleted = []
+def oneDayAgo = new Date() - 1
+jenkins.model.Jenkins.instance.nodes.each { hudson.model.Node node ->
+  node.workspaceRoot.listDirectories().each { hudson.FilePath path ->
+    def pathName = path.getRemote()
+    if (path.name.startsWith(".")) {
+      println "Skipping internal dir $node.displayName:$pathName"
+    } else {
+        def lastModified = new Date(path.lastModified())
+      if (lastModified < oneDayAgo) {
+        println "Deleting workspace at $node.displayName:$pathName (last modified $lastModified)"
+        path.deleteRecursive()
+        deleted << "$node.displayName:$pathName"
+      } else {
+        println "Skipping workspace at $node.displayName:$pathName (last modified $lastModified)"
+      }
+    }
+  }
+}
+"Deleted workspaces: \n\t" + deleted.sort().join("\n\t")
